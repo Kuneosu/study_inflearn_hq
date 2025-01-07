3d6d7d098ceac69ae8fa8daac79b6ded4bd42f1b
Kuneosu <kks92155784@gmail.com>
2025-01-07 15:21:38 +0900

    05 - 폰트 설정

--- inflearn_flutter_hq/lib/ui/text_styles.dart
+++ inflearn_flutter_hq/lib/ui/text_styles.dart
@@ -1,60 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 abstract class TextStyles {
-  static const TextStyle titleTextBold = TextStyle(
+  static TextStyle titleTextBold = GoogleFonts.poppins(
     fontSize: 50,
     fontWeight: FontWeight.bold,
   );
-  static const TextStyle headerTextBold = TextStyle(
+  static TextStyle headerTextBold = GoogleFonts.poppins(
     fontSize: 30,
     fontWeight: FontWeight.bold,
   );
-  static const TextStyle largeTextBold = TextStyle(
+  static TextStyle largeTextBold = GoogleFonts.poppins(
     fontSize: 20,
     fontWeight: FontWeight.bold,
   );
-  static const TextStyle mediumTextBold = TextStyle(
+  static TextStyle mediumTextBold = GoogleFonts.poppins(
     fontSize: 18,
     fontWeight: FontWeight.bold,
   );
-  static const TextStyle normalTextBold = TextStyle(
+  static TextStyle normalTextBold = GoogleFonts.poppins(
     fontSize: 16,
     fontWeight: FontWeight.bold,
   );
-  static const TextStyle smallTextBold = TextStyle(
+  static TextStyle smallTextBold = GoogleFonts.poppins(
     fontSize: 14,
     fontWeight: FontWeight.bold,
   );
-  static const TextStyle smallerTextBold = TextStyle(
+  static TextStyle smallerTextBold = GoogleFonts.poppins(
     fontSize: 11,
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle titleTextRegular = TextStyle(
+  static TextStyle titleTextRegular = GoogleFonts.poppins(
     fontSize: 50,
     fontWeight: FontWeight.normal,
   );
-  static const TextStyle headerTextRegular = TextStyle(
+  static TextStyle headerTextRegular = GoogleFonts.poppins(
     fontSize: 30,
     fontWeight: FontWeight.normal,
   );
-  static const TextStyle largeTextRegular = TextStyle(
+  static TextStyle largeTextRegular = GoogleFonts.poppins(
     fontSize: 20,
     fontWeight: FontWeight.normal,
   );
-  static const TextStyle mediumTextRegular = TextStyle(
+  static TextStyle mediumTextRegular = GoogleFonts.poppins(
     fontSize: 18,
     fontWeight: FontWeight.normal,
   );
-  static const TextStyle normalTextRegular = TextStyle(
+  static TextStyle normalTextRegular = GoogleFonts.poppins(
     fontSize: 16,
     fontWeight: FontWeight.normal,
   );
-  static const TextStyle smallTextRegular = TextStyle(
+  static TextStyle smallTextRegular = GoogleFonts.poppins(
     fontSize: 14,
     fontWeight: FontWeight.normal,
   );
-  static const TextStyle smallerTextRegular = TextStyle(
+  static TextStyle smallerTextRegular = GoogleFonts.poppins(
     fontSize: 11,
     fontWeight: FontWeight.normal,
   );
