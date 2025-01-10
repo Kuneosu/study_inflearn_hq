ae5f135ad0d3760b745d726fc37e5666a64c3d83
Kuneosu <kks92155784@gmail.com>
2025-01-10 17:24:01 +0900

    18 Navigation, StatefulShellRoute

--- inflearn_flutter_hq/lib/core/routing/route_paths.dart
+++ inflearn_flutter_hq/lib/core/routing/route_paths.dart
@@ -2,5 +2,9 @@ abstract class RoutePaths {
   static const splash = '/Splash';
   static const signIn = '/SignIn';
   static const signUp = '/SignUp';
+  
   static const savedRecipes = '/SavedRecipes';
+  static const home = '/Home';
+  static const profile = '/Profile';
+  static const notifications = '/Notifications';
 }
--- inflearn_flutter_hq/lib/core/routing/router.dart
+++ inflearn_flutter_hq/lib/core/routing/router.dart
@@ -1,5 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:inflearn_flutter_hq/core/routing/route_paths.dart';
+import 'package:inflearn_flutter_hq/presentation/home/home_screen.dart';
+import 'package:inflearn_flutter_hq/presentation/main/main_screen.dart';
+import 'package:inflearn_flutter_hq/presentation/notifications/notifications_screen.dart';
+import 'package:inflearn_flutter_hq/presentation/profile/profile_screen.dart';
 import 'package:inflearn_flutter_hq/presentation/saved_recipes/screen/saved_recipes_root.dart';
 import 'package:inflearn_flutter_hq/presentation/sign_in/sign_in_screen.dart';
 import 'package:inflearn_flutter_hq/presentation/sign_up/sing_up_screen.dart';
@@ -18,7 +22,7 @@ final router = GoRouter(
       path: RoutePaths.signIn,
       builder: (context, state) => SignInScreen(
         onSignUp: () => context.go(RoutePaths.signUp),
-        onSignIn: () => context.go(RoutePaths.savedRecipes),
+        onSignIn: () => context.go(RoutePaths.home),
       ),
     ),
     GoRoute(
@@ -27,9 +31,53 @@ final router = GoRouter(
         onSignIn: () => context.go(RoutePaths.signIn),
       ),
     ),
-    GoRoute(
-      path: RoutePaths.savedRecipes,
-      builder: (context, state) => SavedRecipesRoot(),
-    )
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return MainScreen(
+          body: navigationShell,
+          currentPageIndex: navigationShell.currentIndex,
+          onChangeIndex: (index) {
+            navigationShell.goBranch(
+              index,
+              initialLocation: index == navigationShell.currentIndex,
+            );
+          },
+        );
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.home,
+              builder: (context, state) => HomeScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.savedRecipes,
+              builder: (context, state) => SavedRecipesRoot(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.notifications,
+              builder: (context, state) => NotificationsScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.profile,
+              builder: (context, state) => ProfileScreen(),
+            ),
+          ],
+        ),
+      ],
+    ),
   ],
 );
