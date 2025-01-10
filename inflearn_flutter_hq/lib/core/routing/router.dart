689ea71c0a4032f65a88d9b644ee47e491599e1e
Kuneosu <kks92155784@gmail.com>
2025-01-10 16:04:28 +0900

    16 MVVM - SavedRecipesViewModel & Root

--- inflearn_flutter_hq/lib/core/routing/router.dart
+++ inflearn_flutter_hq/lib/core/routing/router.dart
@@ -1,11 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:inflearn_flutter_hq/core/routing/route_paths.dart';
-import 'package:inflearn_flutter_hq/data/repository/mock_bookmark_repository_impl.dart';
-import 'package:inflearn_flutter_hq/data/repository/mock_recipe_repository_impl.dart';
-import 'package:inflearn_flutter_hq/domain/model/recipe.dart';
-import 'package:inflearn_flutter_hq/domain/use_case/get_saved_recipes_use_case.dart';
-import 'package:inflearn_flutter_hq/presentation/saved_recipes/saved_recipes_screen.dart';
+import 'package:inflearn_flutter_hq/presentation/saved_recipes/screen/saved_recipes_root.dart';
 import 'package:inflearn_flutter_hq/presentation/sign_in/sign_in_screen.dart';
 import 'package:inflearn_flutter_hq/presentation/sign_up/sing_up_screen.dart';
 import 'package:inflearn_flutter_hq/presentation/splash/splash_screen.dart';
@@ -34,21 +29,7 @@ final router = GoRouter(
     ),
     GoRoute(
       path: RoutePaths.savedRecipes,
-      builder: (context, state) => FutureBuilder<List<Recipe>>(
-          future: GetSavedRecipesUseCase(
-            recipeRepository: MockRecipeRepositoryImpl(),
-            bookmarkRepository: MockBookmarkRepositoryImpl(),
-          ).execute(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-
-            final recipes = snapshot.data!;
-            return SavedRecipesScreen(
-              recipes: recipes,
-            );
-          }),
+      builder: (context, state) => SavedRecipesRoot(),
     )
   ],
 );
